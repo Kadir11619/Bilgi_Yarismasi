f8850a3f325a5ce673ffeb7763548135de0fa30e
Kadir11619 <82710596+Kadir11619@users.noreply.github.com>
2022-11-04 16:10:23 +0300

    .

--- Bilgi Yarışması/Program.cs
+++ Bilgi Yarışması/Program.cs
@@ -8,6 +8,9 @@ namespace Bilgi_Yarışması
         [STAThread]
         static void Main()
         {
+
+
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
